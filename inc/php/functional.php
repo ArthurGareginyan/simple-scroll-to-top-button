d112ff90fe7072eede30420f3c2dd6a0ee84f9b4
Arthur Gareginyan <arthurgareginyan@gmail.com>
2021-02-13 17:42:04 +0300

    Maintenance: New function ("_load_on") used

--- inc/php/functional.php
+++ inc/php/functional.php
@@ -11,6 +11,12 @@ defined( 'ABSPATH' ) or die( "Restricted access!" );
  */
 function spacexchimp_p008_generator() {
 
+    // Return if the current page does not match the selected one
+    $load_on = spacexchimp_p008_load_on();
+    if ( $load_on !== true ) {
+        return;
+    }
+
     // Put value of plugin constants into an array for easier access
     $plugin = spacexchimp_p008_plugin();
 
@@ -18,22 +24,19 @@ function spacexchimp_p008_generator() {
     $options = get_option( $plugin['settings'] . '_settings' );
 
     // Declare variables
-    $display_on = !empty( $options['display-button'] ) ? $options['display-button'] : '';
     $transparency = (!empty( $options['transparency_button'] ) AND $options['transparency_button'] == 'on') ? 'ssttbutton-transparent' : '' ;
     $background_button = !empty( $options['background_button'] ) ? $options['background_button'] : 'fa-circle';
     $image_button = !empty( $options['image_button'] ) ? $options['image_button'] : 'fa-arrow-up';
 
     // Generate button
-    if ( $display_on == '' OR $display_on == 'Home Page Only' AND is_home() OR $display_on == 'Home Page Only' AND is_front_page() ) {
-        ?>
-            <a id="ssttbutton" href="#top" class="<?php echo $transparency; ?>">
-                <span class="fa-stack fa-lg">
-                    <i class="ssttbutton-background fa <?php echo $background_button; ?> fa-stack-2x"></i>
-                    <i class="ssttbutton-symbol fa <?php echo $image_button; ?> fa-stack-1x"></i>
-                </span>
-            </a>
-        <?php
-    }
+    ?>
+        <a id="ssttbutton" href="#top" class="<?php echo $transparency; ?>">
+            <span class="fa-stack fa-lg">
+                <i class="ssttbutton-background fa <?php echo $background_button; ?> fa-stack-2x"></i>
+                <i class="ssttbutton-symbol fa <?php echo $image_button; ?> fa-stack-1x"></i>
+            </span>
+        </a>
+    <?php
 }
 
 /**
