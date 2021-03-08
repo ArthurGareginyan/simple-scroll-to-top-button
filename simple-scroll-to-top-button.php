8363f64c6d98aba775c735a706416148c75c0e5f
Arthur Gareginyan <arthurgareginyan@gmail.com>
2021-03-09 02:13:31 +0300

    Plugin updated to version 4.44

--- simple-scroll-to-top-button.php
+++ simple-scroll-to-top-button.php
@@ -5,7 +5,7 @@
  * Description: Easily and safely add a "Scroll to Top" button to your WordPress website.
  * Author: Space X-Chimp
  * Author URI: https://www.spacexchimp.com
- * Version: 4.43
+ * Version: 4.44
  * License: GPL3
  * Text Domain: simple-scroll-to-top-button
  * Domain Path: /languages/
