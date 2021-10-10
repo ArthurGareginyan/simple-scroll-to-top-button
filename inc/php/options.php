2bca820d8d3c218d413422b00be3e974a448e732
Arthur Gareginyan <arthurgareginyan@gmail.com>
2021-10-11 01:08:15 +0300

    Maintenance: Processing of options has been improved

--- inc/php/options.php
+++ inc/php/options.php
@@ -15,15 +15,14 @@ function spacexchimp_p008_options() {
     $plugin = spacexchimp_p008_plugin();
 
     // Retrieve options from database
-    $options = get_option( $plugin['settings'] . '_settings' );
+    $array = get_option( $plugin['settings'] . '_settings' );
 
     // Make the "$options" array if the plugin options data in the database is not exist
-    if ( ! is_array( $options ) ) {
-        $options = array();
+    if ( ! is_array( $array ) ) {
+        $array = array();
     }
 
     // Create an array with options
-    $array = $options;
     $list = array(
         'background_button' => (string) 'fa-circle', // _control_choice
         'background-color' => (string) '#ff4f7d', // _control_color
@@ -38,7 +37,7 @@ function spacexchimp_p008_options() {
     foreach ( $list as $name => $default ) {
 
         // Set default value if option is empty
-        $array[$name] = !empty( $options[$name] ) ? $options[$name] : $default;
+        $array[$name] = !empty( $array[$name] ) ? $array[$name] : $default;
 
         // Cast and validate by type of option
         if ( is_string( $default ) === true ) {
